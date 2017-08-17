89755243b27ded56e3bfc7b5bec186ea46c62954
Mohammad Chavoshi <Mohammad.Chavoshi@hotmail.com>
2017-08-17 17:07:15 +0800

    Add MySql Example

---
+++ Examples/EnterpriseLibrary.Test.MySql/EnterpriseLibrary.Test.MySql/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Data;
+using System.Data.Common;
+
+namespace EnterpriseLibrary.Test.MySql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string sqlCmd = @"INSERT INTO City
+                                    (
+                                        CityID,
+	                                    Name
+                                    )
+                                    VALUES
+                                    (
+                                        @CityID,
+	                                    @Name
+                                    )";
+
+            DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory(new SystemConfigurationSource(false).GetSection), false);
+            Database db = DatabaseFactory.CreateDatabase("BizDbMySql");
+
+            using (DbCommand cmd = db.GetSqlStringCommand(sqlCmd))
+            {
+                try
+                {
+                    db.AddInParameter(cmd, "@CityID", DbType.Int32, 2);
+                    db.AddInParameter(cmd, "@Name", DbType.String, "London");
+                    db.ExecuteNonQuery(cmd);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
