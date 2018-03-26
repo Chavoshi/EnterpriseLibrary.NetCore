e60744441432e9f5cb6ec82ba1b29d474bc7635e
Mohammad Chavoshi <Mohammad.Chavoshi@hotmail.com>
2018-03-26 18:17:06 +0800

    EnterpriseLibrary.Data nuget package with Oracle and Odbc support added

--- Examples/EnterpriseLibrary.Test.MySql/EnterpriseLibrary.Test.MySql/Program.cs
+++ Examples/EnterpriseLibrary.Test.MySql/EnterpriseLibrary.Test.MySql/Program.cs
@@ -10,6 +10,43 @@ namespace EnterpriseLibrary.Test.MySql
     {
         static void Main(string[] args)
         {
+            TestOracle();
+            TestMySql();
+        }
+
+        private static void TestOracle()
+        {
+            const string sqlCmd = @"INSERT INTO CITY
+                                    (
+                                        CITYID,
+	                                    NAME
+                                    )
+                                    VALUES
+                                    (
+                                        :CityID,
+	                                    :Name
+                                    )";
+
+            DatabaseFactory.SetDatabaseProviderFactory(new DatabaseProviderFactory(new SystemConfigurationSource(false).GetSection), false);
+            Database db = DatabaseFactory.CreateDatabase("BizDbOracle");
+
+            using (DbCommand cmd = db.GetSqlStringCommand(sqlCmd))
+            {
+                try
+                {
+                    db.AddInParameter(cmd, "CITYID", DbType.Int32, 2);
+                    db.AddInParameter(cmd, "NAME", DbType.String, "London");
+                    db.ExecuteNonQuery(cmd);
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static void TestMySql()
+        {
             const string sqlCmd = @"INSERT INTO City
                                     (
                                         CityID,
